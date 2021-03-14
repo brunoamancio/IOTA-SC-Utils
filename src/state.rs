3de0c80fe6ec88815443bb7b3be18a661efec0bf
Th3B0Y <brunoamancio.ti@gmail.com>
2021-03-14 17:15:36 +0100

    Create state.rs

---
+++ src/state.rs
@@ -0,0 +1,18 @@
+use wasmlib::*;
+use crate::getter::Getter;
+use crate::getter::STATE;
+
+// Primitive types
+add_impl_pub_fns!(STATE, must_get_string, get_string, exists_string, String);
+add_impl_pub_fns!(STATE, must_get_int64, get_int64, exists_int64, i64);
+add_impl_pub_fns!(STATE, must_get_bytes, get_bytes, exists_bytes, Vec<u8>);
+
+// ISCP Types
+add_impl_pub_fns!(STATE, must_get_agent_id, get_agent_id, exists_agent_id, ScAgentId);
+add_impl_pub_fns!(STATE, must_get_address, get_address, exists_address, ScAddress);
+add_impl_pub_fns!(STATE, must_get_request_id, get_request_id, exists_request_id, ScRequestId);
+add_impl_pub_fns!(STATE, must_get_hname, get_hname, exists_hname, ScHname);
+add_impl_pub_fns!(STATE, must_get_hash, get_hash, exists_hash, ScHash);
+add_impl_pub_fns!(STATE, must_get_contract_id, get_contract_id, exists_contract_id, ScContractId);
+add_impl_pub_fns!(STATE, must_get_color, get_color, exists_color, ScColor);
+add_impl_pub_fns!(STATE, must_get_chain_id, get_chain_id, exists_chain_id, ScChainId);
