4dd144527cebf8c1b889addf8f6d59569dc08327
Th3B0Y <brunoamancio.ti@gmail.com>
2021-03-14 17:15:12 +0100

    Create params.rs

---
+++ src/params.rs
@@ -0,0 +1,54 @@
+use wasmlib::*;
+use crate::getter::Getter;
+use crate::getter::PARAMS;
+
+pub fn new() -> ScMutableMap {
+    let params = ScMutableMap::new();
+    params
+}
+
+// ---------------------------    Getter functions    -------------------------------------
+
+// Primitive types
+add_impl_pub_fns!(PARAMS, must_get_string, get_string, exists_string, String);
+add_impl_pub_fns!(PARAMS, must_get_int64, get_int64, exists_int64, i64);
+add_impl_pub_fns!(PARAMS, must_get_bytes, get_bytes, exists_bytes, Vec<u8>);
+
+// ISCP Types
+add_impl_pub_fns!(PARAMS, must_get_agent_id, get_agent_id, exists_agent_id, ScAgentId);
+add_impl_pub_fns!(PARAMS, must_get_address, get_address, exists_address, ScAddress);
+add_impl_pub_fns!(PARAMS, must_get_request_id, get_request_id, exists_request_id, ScRequestId);
+add_impl_pub_fns!(PARAMS, must_get_hname, get_hname, exists_hname, ScHname);
+add_impl_pub_fns!(PARAMS, must_get_hash, get_hash, exists_hash, ScHash);
+add_impl_pub_fns!(PARAMS, must_get_contract_id, get_contract_id, exists_contract_id, ScContractId);
+add_impl_pub_fns!(PARAMS, must_get_color, get_color, exists_color, ScColor);
+add_impl_pub_fns!(PARAMS, must_get_chain_id, get_chain_id, exists_chain_id, ScChainId);
+
+
+// ---------------------------    Add functions    -------------------------------------
+
+macro_rules! add_impl_adder_pub_fns {
+
+    ($add_func_name:ident, $get_func : ident, $param_type : ty) => {
+        /// Tries to get a variable. Returns default value if it can't find it.
+        pub fn $add_func_name(key :&str, value : $param_type, mutable_map : &ScMutableMap) {
+            let hname_param = mutable_map.$get_func(key);
+            hname_param.set_value(value);
+        }
+    };
+}
+
+// Primitive types
+add_impl_adder_pub_fns!(add_string, get_string, &str);
+add_impl_adder_pub_fns!(add_int64, get_int64, i64);
+add_impl_adder_pub_fns!(add_bytes, get_bytes, &[u8]);
+
+// ISCP Types
+add_impl_adder_pub_fns!(add_agent_id, get_agent_id, &ScAgentId);
+add_impl_adder_pub_fns!(add_address, get_address, &ScAddress);
+add_impl_adder_pub_fns!(add_request_id, get_request_id, &ScRequestId);
+add_impl_adder_pub_fns!(add_hname, get_hname, ScHname);
+add_impl_adder_pub_fns!(add_hash, get_hash, &ScHash);
+add_impl_adder_pub_fns!(add_contract_id, get_contract_id, &ScContractId);
+add_impl_adder_pub_fns!(add_color, get_color, &ScColor);
+add_impl_adder_pub_fns!(add_chain_id, get_chain_id, &ScChainId);
