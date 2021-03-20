47199d37a8da37342ae943465044bd4bd345812c
Th3B0Y <brunoamancio.ti@gmail.com>
2021-03-20 22:00:15 +0100

    Renamed constants in interfaces module.

--- src/interfaces.rs
+++ src/interfaces.rs
@@ -1,14 +1,14 @@
 use crate::{params, results, generator, wasmlib::{ScHname, ScViewContext}};
 
 /// Name of the function responsible for checking if contracts implement interfaces, as defined by the TIP-100 standard.
-pub const NAME_FUNC_IMPLEMENTS : &str  = "implements";
-// Hash of the name of the function "implements" described in TIP-100.
-const HNAME_FUNC_IMPLEMENTS : ScHname  =  generator::generate_schname!("implements");
+pub const NAME_VIEW_IMPLEMENTS : &str  = "implements";
+// Hash of the name of the view "implements" described in TIP-100.
+const HNAME_VIEW_IMPLEMENTS : ScHname  =  generator::generate_schname!("implements");
 
 /// Name of the TIP-100 interface.
 pub const INTERFACE_TIP_100 : &str = "tip_100";
 /// Hash of TIP-100 Interface.
-pub const HNAME_INTERFACE_TIP_100 : ScHname = generator::generate_schname!("implements(ScHname,ScHname)->bool");
+pub const HNAME_INTERFACE_TIP_100 : ScHname = generator::generate_schname!("implements(ScHname)->bool");
 
 /// Function which calls the contract with hname `hname_contract` and checks if it implements interface `hname_interface`.
 /// # Sample:
@@ -26,7 +26,7 @@ pub fn implements(hname_contract : ScHname, hname_interface : ScHname, ctx : &Sc
     let input_params = params::new();
     params::add_hname(INTERFACE_TIP_100, hname_interface, &input_params);
 
-    let result = ctx.call(hname_contract, HNAME_FUNC_IMPLEMENTS, Some(input_params));
-    let implements = results::get_bool(NAME_FUNC_IMPLEMENTS, result);
+    let result = ctx.call(hname_contract, HNAME_VIEW_IMPLEMENTS, Some(input_params));
+    let implements = results::get_bool(NAME_VIEW_IMPLEMENTS, result);
     implements
 }
